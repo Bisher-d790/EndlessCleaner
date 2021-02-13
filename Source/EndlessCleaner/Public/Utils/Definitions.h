b63232f8eded9cccd06418d96b78bdb272614ee0
BISHER-PC\Bisher Dokkmak <bisher.d790@gmail.com>
2021-02-14 03:23:16 +0800

    [Fix] Remove unused getters in Utils where it creates build-time error for incompatible references. [Setup] Setup Android Packaging settings. [Edit] Reduce Pacakge size by optimizing settings, Removing unused plugins, Removing unused Assets, disabling Starter Content.

--- Source/EndlessCleaner/Public/Utils/Definitions.h
+++ Source/EndlessCleaner/Public/Utils/Definitions.h
@@ -27,27 +27,3 @@ enum class EGameState : uint8
 	VE_Respawn				UMETA(DisplayName = "Respawn"),
 	VE_GameOver				UMETA(DisplayName = "GameOver")
 };
-
-template<typename TEnum>
-static FORCEINLINE FString GetEnumValueAsString(const FString& Name, TEnum Value)
-{
-	const UEnum* enumPtr = FindObject<UEnum>(ANY_PACKAGE, *Name, true);
-	if (!enumPtr)
-	{
-		return FString("Invalid");
-	}
-
-	return enumPtr->GetNameByValue((int64)Value).ToString();
-}
-
-template<typename EnumType>
-static FORCEINLINE EnumType GetEnumValueFromString(const FString& EnumName, const FString& String)
-{
-	UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, *EnumName, true);
-	if (!Enum)
-	{
-		return FString("Invalid");
-	}
-
-	return Enum->GetValueByName(String);
-}
