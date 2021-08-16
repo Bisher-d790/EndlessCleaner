903180149326870965792bf2557581f3509919eb
Bisher Dokkmak <bisher.d790@gmail.com>
2021-08-16 23:05:00 +0800

    [Setup] Waypoint Movement Component: Setup the waypoint movement component functionality with local space support.

--- Source/EndlessCleaner/Private/Gameplay/Components/WaypointMovementComponent.cpp
+++ Source/EndlessCleaner/Private/Gameplay/Components/WaypointMovementComponent.cpp
@@ -3,3 +3,56 @@
 
 #include "Gameplay/Components/WaypointMovementComponent.h"
 
+UWaypointMovementComponent::UWaypointMovementComponent()
+{
+	MovementSpeed = 10;
+	bMovementInLocalSpace = true;
+}
+
+void UWaypointMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (bMovementInLocalSpace)
+	{
+		FVector ActorLocation = UpdatedComponent->GetComponentLocation();
+		for (int i = 0; i < WaypointLocations.Num(); i++)
+		{
+			WaypointLocations[i] += ActorLocation;
+		}
+	}
+}
+
+void UWaypointMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Skip if there're no Waypoints, or there's only one waypoint
+	if (WaypointLocations.Num() <= 1) return;
+
+	// skip if we don't want component updated when not rendered or if updated component can't move
+	if (ShouldSkipUpdate(DeltaTime)) return;
+	if (!IsValid(UpdatedComponent))	return;
+
+	// Compute new location
+	FVector CurrentLocation = UpdatedComponent->GetComponentLocation();
+	FVector TargetLocation = WaypointLocations[NextIndex];
+
+	// Movement time duration = distance / speed
+	float LerpDuration = FVector::Dist(CurrentLocation, TargetLocation) / MovementSpeed;
+	FVector NewLocation = FMath::Lerp(CurrentLocation, TargetLocation, LerpTimeElapsed / LerpDuration);
+
+	LerpTimeElapsed += DeltaTime;
+
+	UpdatedComponent->SetRelativeLocation(NewLocation);
+
+	if (LerpTimeElapsed >= LerpDuration) NextWaypoint();
+}
+
+void UWaypointMovementComponent::NextWaypoint()
+{
+	NextIndex++;
+	if (NextIndex >= WaypointLocations.Num()) NextIndex = 0;
+
+	LerpTimeElapsed = 0;
+}
--- Source/EndlessCleaner/Public/Gameplay/Components/WaypointMovementComponent.h
+++ Source/EndlessCleaner/Public/Gameplay/Components/WaypointMovementComponent.h
@@ -7,11 +7,37 @@
 #include "WaypointMovementComponent.generated.h"
 
 /**
- * 
+ * Moves the actor across multiple waypoint location points.
  */
-UCLASS()
+UCLASS(ClassGroup = Movement, meta = (BlueprintSpawnableComponent), HideCategories = (Velocity))
 class ENDLESSCLEANER_API UWaypointMovementComponent : public UMovementComponent
 {
 	GENERATED_BODY()
-	
+
+private:
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UWaypointMovementComponent();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+protected:
+	// The rotation of the wave start
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = WaypointComponent)
+		TArray<FVector> WaypointLocations = TArray<FVector>();
+
+	// The speed of the rotation
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = WaypointComponent)
+		float MovementSpeed;
+
+	// Whether movement is applied in local or world space.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = WaypointComponent)
+		uint32 bMovementInLocalSpace : 1;
+
+	void NextWaypoint();
+
+	int NextIndex = 0;
+
+	float LerpTimeElapsed = 0;
 };
