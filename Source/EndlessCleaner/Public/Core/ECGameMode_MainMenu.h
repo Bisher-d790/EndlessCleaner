7da330ad7b1600ba6d411c87a61e9b0fe369e6ac
Bisher Dokkmak <bisher.d790@gmail.com>
2022-01-18 02:52:02 +0800

    [Setup] Main Menu: Setup MainMenu screen with basic Play and Credits buttons.

---
+++ Source/EndlessCleaner/Public/Core/ECGameMode_MainMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "ECGameMode_MainMenu.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ENDLESSCLEANER_API AECGameMode_MainMenu : public AGameMode
+{
+	GENERATED_BODY()
+
+};
