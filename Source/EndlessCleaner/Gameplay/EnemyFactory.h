f26e82d846f660e2627cd3adf9c3a8de8ab47854
Bisher Dokkmak <35769802+Bisher-d790@users.noreply.github.com>
2022-11-26 22:22:24 +0700

    [Fix] Enemy Factory: Remove virtual from class with no virtual destrcutor.

--- Source/EndlessCleaner/Gameplay/EnemyFactory.h
+++ Source/EndlessCleaner/Gameplay/EnemyFactory.h
@@ -12,11 +12,11 @@ class ENDLESSCLEANER_API EnemyFactory
 public:
 	static EnemyFactory* GetInstance(UWorld* World);
 
-	virtual class AEnemy* CreateEnemy(TSubclassOf<AEnemy> EnemyClass, FVector SpawnPosition, float EnemyStartSpeed, float EnemyActualSpeed, float EnemyDistanceToRetract);
+	class AEnemy* CreateEnemy(TSubclassOf<AEnemy> EnemyClass, FVector SpawnPosition, float EnemyStartSpeed, float EnemyActualSpeed, float EnemyDistanceToRetract);
 
-	virtual void DestroyEnemy(int EnemyIndex);
+	void DestroyEnemy(int EnemyIndex);
 
-	virtual void DestroyEnemy(class AEnemy* KilledEnemy);
+	void DestroyEnemy(class AEnemy* KilledEnemy);
 
 	void DestroyAllEnemies();
 
